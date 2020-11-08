8d6c0612ee0c8d5224e2737f64505018f0394c17
Caique Guedes <caique-guedes@hotmail.com>
2020-11-08 08:21:09 -0300

    :memo: configured constants

--- app/src/main/java/br/com/likeflix/Constants.kt
+++ app/src/main/java/br/com/likeflix/Constants.kt
@@ -1,3 +1,8 @@
 package br.com.likeflix
 
-const val BASE_URL = "https://bank-app-test.herokuapp.com/api/"
+const val API_KEY = "14c57a42aad2b94c3b56b485834ec4a6"
+const val DEFAULT_LANGUAGE = "pt-BR"
+const val BASE_URL = "https://api.themoviedb.org/3/"
+const val BASE_URL_IMG = "http://image.tmdb.org/t/p/w200//"
+const val VIEW_TYPE_ITEM = 0
+const val VIEW_TYPE_LOADING = 1
